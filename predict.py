3aef06965290cee5d29b8af85825c2b21fc80b75
Hickey-Curry <76723462+Hickey-Curry@users.noreply.github.com>
2023-05-17 14:42:30 +0800

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,77 @@
+
+import time
+
+import cv2
+import numpy as np
+from PIL import Image
+
+from yolo import YOLO
+
+if __name__ == "__main__":
+    yolo = YOLO()
+    #----------------------------------------------------------------------------------------------------------#
+    #   mode    Used to specify the mode of the test：
+    #   'predict'           If you want to modify the prediction process, such as saving pictures, intercepting objects, etc.
+    #   'fps'               Indicates the test FPS
+    #   'dir_predict'       Represents traversing the folder to detect and save.
+    #   'heatmap'           Represents a heatmap visualization of the results of making predictions
+    #----------------------------------------------------------------------------------------------------------#
+    mode = "fps"
+
+    crop            = False
+    count           = False
+
+    test_interval   = 100
+    fps_image_path  = "img/1.jpg"
+
+    dir_origin_path = "img/"
+    dir_save_path   = "img_out/"
+
+    heatmap_save_path = "model_data/heatmap_vision.png"
+
+
+    if mode == "predict":
+        while True:
+            img = input('Input image filename:')
+            try:
+                image = Image.open(img)
+            except:
+                print('Open Error! Try again!')
+                continue
+            else:
+                r_image = yolo.detect_image(image, crop = crop, count=count)
+                r_image.show()
+
+    elif mode == "fps":
+        img = Image.open(fps_image_path)
+        tact_time = yolo.get_FPS(img, test_interval)
+        print(str(tact_time) + ' seconds, ' + str(1/tact_time) + 'FPS, @batch_size 1')
+
+    elif mode == "dir_predict":
+        import os
+
+        from tqdm import tqdm
+
+        img_names = os.listdir(dir_origin_path)
+        for img_name in tqdm(img_names):
+            if img_name.lower().endswith(('.bmp', '.dib', '.png', '.jpg', '.jpeg', '.pbm', '.pgm', '.ppm', '.tif', '.tiff')):
+                image_path  = os.path.join(dir_origin_path, img_name)
+                image       = Image.open(image_path)
+                r_image     = yolo.detect_image(image)
+                if not os.path.exists(dir_save_path):
+                    os.makedirs(dir_save_path)
+                r_image.save(os.path.join(dir_save_path, img_name.replace(".jpg", ".png")), quality=95, subsampling=0)
+
+    elif mode == "heatmap":
+        while True:
+            img = input('Input image filename:')
+            try:
+                image = Image.open(img)
+            except:
+                print('Open Error! Try again!')
+                continue
+            else:
+                yolo.detect_heatmap(image, heatmap_save_path)
+        
+    else:
+        raise AssertionError("Please specify the correct mode: 'predict', 'video', 'fps', 'heatmap', 'export_onnx', 'dir_predict'.")
